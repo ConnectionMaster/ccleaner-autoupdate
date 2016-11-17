02b294ce133b6c30e4b2fbcc58af9c84a81ae232
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2016-11-17 18:37:52 -0500

    updated addtask.bat
    now dectects windows xp or newer versions of windows

--- bin/addtask.bat
+++ bin/addtask.bat
@@ -1,21 +1,31 @@
 @echo off
 cd /d "%~dp0"
-cd ..
 :: http://superuser.com/questions/243605/how-do-i-specify-run-with-highest-privileges-in-schtasks
 setlocal
-set runlevel=
+REM set runlevel=
 
-REM Get OS version from registry
+:: Get OS version from registry
 for /f "tokens=2*" %%i in ('reg.exe query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "CurrentVersion"') do set os_ver=%%j
 
-REM Set run level (for Vista or later - version 6)
-if /i "%os_ver:~,1%" GEQ "6" set runlevel=/rl HIGHEST
+:: Set run level (for Vista or later - version 6)
+REM if /i "%os_ver:~,1%" GEQ "6" set runlevel=/rl HIGHEST
 
 REM Execute SchTasks.exe
 REM SchTasks /Create /SC MONTHLY /D 1 /TN "CCleaner Update" /TR "'%cd%\update.bat'" /ST 14:00 /RU SYSTEM %runlevel%
+:: This one for XP
 REM SchTasks /Create /SC DAILY /MO 30 /TN "CCleaner Update test" /TR "'%cd%\update.bat'" /ST 14:00 /RU SYSTEM %runlevel%
 REM SchTasks /Create /SC MONTHLY /D 1 /TN "CCleaner Update" /TR "'%cd%\update.bat'" /ST 14:00 %runlevel%
 REM echo "%cd%\update.bat"
-@echo on	
-schtasks /create /xml "%cd%\bin\ccleanerupdate.xml" /tn "ccleaner test"
+
+
+if /i "%os_ver:~,1%" GEQ "6" (
+	REM echo win 6+ detected
+	:: This one for Vista or later
+	schtasks /create /xml "%cd%\ccleanerupdate.xml" /tn "CCleaner Update"
+) else (
+	REM echo winxp detected
+	:: This one for XP
+	cd ..
+	SchTasks /Create /SC DAILY /MO 30 /TN "CCleaner Update" /TR "'%cd%\update.bat'" /ST 14:00 /RU SYSTEM
+)
 pause
