378af3af4bbca52e0e4f97142f4cf29a91828637
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2017-02-02 00:33:48 -0500

    betastandarddownloader.bat now uses timestamping in wget. Updated download link variable.
    wget timestamping is mostly for debugging purpsoes when we dont delete
    the "standard" file that is downloaded.

--- BETAstandarddownloader.bat
+++ BETAstandarddownloader.bat
@@ -2,7 +2,7 @@ REM this script has a snippet to download the standard installer for when the sl
 @echo off
 cd /d "%~dp0"
 ::first we download the header from the standard download site which has the redirect url
-bin\wget --content-disposition http://www.piriform.com/ccleaner/download/standard
+bin\wget --content-disposition -N -S http://www.piriform.com/ccleaner/download/standard
 
 ::now we look for the redirect url in the header file we downloaded
 ::http://stackoverflow.com/questions/22198458/find-text-in-file-and-set-it-as-a-variable-batch-file
@@ -12,11 +12,18 @@ for /F "delims=" %%a in ('findstr /I "data-download-url" standard') do set "link
 ::skip the first 31 characters and continue for 43 characters to get the download url
 
 echo stage 2, downloading the standard installer
-bin\wget -nc %link:~65,43%
+REM bin\wget -nc %link:~65,43%
+echo.
+:: remove indentations (from the remove spaces section of this link)
+:: http://www.dostips.com/DtTipsStringManipulation.php
+set "link=%link:	=%"
+REM echo."%link%"
+echo %link:~63,43%
+pause
 REM [DEBUG] echo %link:~65,43%
 REM [DEBUG] pause
 ::cleanup the header file
-del standard
+REM del standard
 
 rem installing would be the same as the slim installer
 rem ccsetupxxx.exe /S
